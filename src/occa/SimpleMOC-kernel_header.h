de318e69e9519061c6858a5f0ebabc83403e93c2
Ron Rahaman <rahaman@cs.uchicago.edu>
2015-01-25 23:52:01 -0600

    fixed makefile

--- src/occa/SimpleMOC-kernel_header.h
+++ src/occa/SimpleMOC-kernel_header.h
@@ -16,71 +16,54 @@
 #include<assert.h>
 #include<sys/time.h>
 
-
-#define CUDA_ERROR_CHECK
- 
-#define CudaCheckError()    __cudaCheckError( __FILE__, __LINE__ )
-
-// CUDA Error Handling Macro
-#define CUDA_CALL(x) do { if((x) != cudaSuccess) { \
-    printf("Error at %s:%d\n",__FILE__,__LINE__); \
-    return EXIT_FAILURE;}} while(0)
-
 // User inputs
 typedef struct{
-	int source_regions;
-	int course_axial_intervals;
-	int fine_axial_intervals;
-	long segments;
-	int egroups;
-	int nthreads;
-	int streams;
+  int source_regions;
+  int course_axial_intervals;
+  int fine_axial_intervals;
+  long segments;
+  int egroups;
+  int nthreads;
+  int streams;
 } Input;
 
 // Source Region Structure
 typedef struct{
-	long fine_flux_id;
-	long fine_source_id;
-	long sigT_id;
+  long fine_flux_id;
+  long fine_source_id;
+  long sigT_id;
 } Source;
 
 // Source Arrays
 typedef struct{
-	float * fine_flux_arr;
-	float * fine_source_arr;
-	float * sigT_arr;
+  float * fine_flux_arr;
+  float * fine_source_arr;
+  float * sigT_arr;
 } Source_Arrays;
 
 // Device Source Arrays
 typedef struct{
-	occaMemory fine_flux_arr;
-	occaMemory fine_source_arr;
-	occaMemory sigT_arr;
+  occaMemory fine_flux_arr;
+  occaMemory fine_source_arr;
+  occaMemory sigT_arr;
 } OCCA_Source_Arrays;
 
 // Table structure for computing exponential
 typedef struct{
-	float values[706];
-	float dx;
-	float maxVal;
-	int N;
+  float values[706];
+  float dx;
+  float maxVal;
+  int N;
 } Table;
 
 // kernel.c
-__global__ void run_kernel( Input I, Source * S,
-		Source_Arrays SA, Table * table, unsigned long * state,
-		float * state_fluxes, int N_state_fluxes);
-__device__ void interpolateTable(Table * table, float x, float * out);
-__device__ void LCG_RNG( unsigned long * state );
 
 // init.c
 double mem_estimate( Input I );
-__global__ void	init_flux_states( float * flux_states, int N_flux_states, Input I, unsigned long * state);
 Source * initialize_sources( Input I, Source_Arrays * SA );
 occaMemory initialize_occa_sources( Input I, Source_Arrays * SA_h, OCCA_Source_Arrays * SA_d, Source * sources_h, occaDevice device );
 Table buildExponentialTable( void );
 Input set_default_input( void );
-void __cudaCheckError( const char *file, const int line );
 
 // io.c
 void logo(int version);
--- src/occa/init.c
+++ src/occa/init.c
@@ -3,151 +3,128 @@
 // Gets I from user and sets defaults
 Input set_default_input( void )
 {
-	Input I;
+  Input I;
 
-	I.source_regions = 2250;
-	I.course_axial_intervals = 9;
-	I.fine_axial_intervals = 5;
-	I.segments = 50000000;
-	I.egroups = 100;
-	I.streams = 10000;
+  I.source_regions = 2250;
+  I.course_axial_intervals = 9;
+  I.fine_axial_intervals = 5;
+  I.segments = 50000000;
+  I.egroups = 100;
+  I.streams = 10000;
 
-	return I;
+  return I;
 }
 
 // Returns a memory esimate (in MB) for the program's primary data structures
 double mem_estimate( Input I )
 {
-	size_t nbytes = 0;
+  size_t nbytes = 0;
 
-	// Sources Array
-	nbytes += I.source_regions * sizeof(Source);
+  // Sources Array
+  nbytes += I.source_regions * sizeof(Source);
 
-	// Fine Source Data
-	long N_fine = I.source_regions * I.fine_axial_intervals * I.egroups;
-	nbytes += N_fine * sizeof(float);
+  // Fine Source Data
+  long N_fine = I.source_regions * I.fine_axial_intervals * I.egroups;
+  nbytes += N_fine * sizeof(float);
 
-	// Fine Flux Data
-	nbytes += N_fine * sizeof(float);
+  // Fine Flux Data
+  nbytes += N_fine * sizeof(float);
 
-	// SigT Data
-	long N_sigT = I.source_regions * I.egroups;
-	nbytes += N_sigT * sizeof(float);
+  // SigT Data
+  long N_sigT = I.source_regions * I.egroups;
+  nbytes += N_sigT * sizeof(float);
 
-	// Return MB
-	return (double) nbytes / 1024.0 / 1024.0;
+  // Return MB
+  return (double) nbytes / 1024.0 / 1024.0;
 }
 
 Source * initialize_sources( Input I, Source_Arrays * SA )
 {
-	// Source Data Structure Allocation
-	Source * sources = (Source *) malloc( I.source_regions * sizeof(Source));
+  // Source Data Structure Allocation
+  Source * sources = (Source *) malloc( I.source_regions * sizeof(Source));
 
-	// Allocate Fine Source Data
-	long N_fine = I.source_regions * I.fine_axial_intervals * I.egroups;
-	SA->fine_source_arr = (float *) malloc( N_fine * sizeof(float));
-	for( int i = 0; i < I.source_regions; i++ )
-		sources[i].fine_source_id = i*I.fine_axial_intervals*I.egroups;
+  // Allocate Fine Source Data
+  long N_fine = I.source_regions * I.fine_axial_intervals * I.egroups;
+  SA->fine_source_arr = (float *) malloc( N_fine * sizeof(float));
+  for( int i = 0; i < I.source_regions; i++ )
+    sources[i].fine_source_id = i*I.fine_axial_intervals*I.egroups;
 
-	// Allocate Fine Flux Data
-	SA->fine_flux_arr = (float *) malloc( N_fine * sizeof(float));
-	for( int i = 0; i < I.source_regions; i++ )
-		sources[i].fine_flux_id = i*I.fine_axial_intervals*I.egroups;
+  // Allocate Fine Flux Data
+  SA->fine_flux_arr = (float *) malloc( N_fine * sizeof(float));
+  for( int i = 0; i < I.source_regions; i++ )
+    sources[i].fine_flux_id = i*I.fine_axial_intervals*I.egroups;
 
-	// Allocate SigT Data
-	long N_sigT = I.source_regions * I.egroups;
-	SA->sigT_arr = (float *) malloc( N_sigT * sizeof(float));
-	for( int i = 0; i < I.source_regions; i++ )
-		sources[i].sigT_id = i * I.egroups;
+  // Allocate SigT Data
+  long N_sigT = I.source_regions * I.egroups;
+  SA->sigT_arr = (float *) malloc( N_sigT * sizeof(float));
+  for( int i = 0; i < I.source_regions; i++ )
+    sources[i].sigT_id = i * I.egroups;
 
-	// Initialize fine source and flux to random numbers
-	for( long i = 0; i < N_fine; i++ )
-	{
-		SA->fine_source_arr[i] = rand() / RAND_MAX;
-		SA->fine_flux_arr[i] = rand() / RAND_MAX;
-	}
+  // Initialize fine source and flux to random numbers
+  for( long i = 0; i < N_fine; i++ )
+  {
+    SA->fine_source_arr[i] = rand() / RAND_MAX;
+    SA->fine_flux_arr[i] = rand() / RAND_MAX;
+  }
 
-	// Initialize SigT Values
-	for( int i = 0; i < N_sigT; i++ )
-		SA->sigT_arr[i] = rand() / RAND_MAX;
+  // Initialize SigT Values
+  for( int i = 0; i < N_sigT; i++ )
+    SA->sigT_arr[i] = rand() / RAND_MAX;
 
-	return sources;
+  return sources;
 }
 
 occaMemory initialize_occa_sources( Input I, Source_Arrays * SA_h, OCCA_Source_Arrays * SA_d, Source * sources_h, occaDevice device )
 {
-	// Allocate & Copy Fine Source Data
-	long N_fine = I.source_regions * I.fine_axial_intervals * I.egroups;
+  // Allocate & Copy Fine Source Data
+  long N_fine = I.source_regions * I.fine_axial_intervals * I.egroups;
   SA_d->fine_source_arr = occaDeviceMalloc(device, N_fine * sizeof(float), SA_h->fine_source_arr)
 
-	// Allocate & Copy Fine Flux Data
-  SA_d->fine_flux_arr = occaDeviceMalloc(device, N_fine * sizeof(float), SA_h->fine_flux_arr);
+    // Allocate & Copy Fine Flux Data
+    SA_d->fine_flux_arr = occaDeviceMalloc(device, N_fine * sizeof(float), SA_h->fine_flux_arr);
 
-	// Allocate & Copy SigT Data
-	long N_sigT = I.source_regions * I.egroups;
+  // Allocate & Copy SigT Data
+  long N_sigT = I.source_regions * I.egroups;
   SA_d->sigT_arr = occaDeviceMalloc(device, N_sigT * sizeof(float), SA_h->sigT_arr);
 
-	// Allocate & Copy Source Array Data
-	occaMemory sources_d;
+  // Allocate & Copy Source Array Data
+  occaMemory sources_d;
   sources_d = occaDeviceMalloc(device, I.source_regions * sizeof(Source), sources_h);
 
-	return sources_d;
+  return sources_d;
 }
 
 // Builds a table of exponential values for linear interpolation
 Table buildExponentialTable( void )
 {
-	// define table
-	Table table;
+  // define table
+  Table table;
 
-	//float precision = 0.01;
-	float maxVal = 10.0;	
+  //float precision = 0.01;
+  float maxVal = 10.0;	
 
-	// compute number of arry values
-	//int N = (int) ( maxVal * sqrt(1.0 / ( 8.0 * precision * 0.01 ) ) );
-	int N = 353; 
+  // compute number of arry values
+  //int N = (int) ( maxVal * sqrt(1.0 / ( 8.0 * precision * 0.01 ) ) );
+  int N = 353; 
 
-	// compute spacing
-	float dx = maxVal / (float) N;
+  // compute spacing
+  float dx = maxVal / (float) N;
 
-	// store linear segment information (slope and y-intercept)
-	for( int n = 0; n < N; n++ )
-	{
-		// compute slope and y-intercept for ( 1 - exp(-x) )
-		float exponential = exp( - n * dx );
-		table.values[ 2*n ] = - exponential;
-		table.values[ 2*n + 1 ] = 1 + ( n * dx - 1 ) * exponential;
-	}
+  // store linear segment information (slope and y-intercept)
+  for( int n = 0; n < N; n++ )
+  {
+    // compute slope and y-intercept for ( 1 - exp(-x) )
+    float exponential = exp( - n * dx );
+    table.values[ 2*n ] = - exponential;
+    table.values[ 2*n + 1 ] = 1 + ( n * dx - 1 ) * exponential;
+  }
 
-	// assign data to table
-	table.dx = dx;
-	table.maxVal = maxVal - table.dx;
-	table.N = N;
+  // assign data to table
+  table.dx = dx;
+  table.maxVal = maxVal - table.dx;
+  table.N = N;
 
-	return table;
+  return table;
 }
-
-void __cudaCheckError( const char *file, const int line )
-{
-#ifdef CUDA_ERROR_CHECK
-    cudaError err = cudaGetLastError();
-    if ( cudaSuccess != err )
-    {
-        fprintf( stderr, "cudaCheckError() failed at %s:%i : %s\n",
-                 file, line, cudaGetErrorString( err ) );
-        exit( -1 );
-    }
- 
-    // More careful checking. However, this will affect performance.
-    // Comment away if needed.
-    err = cudaDeviceSynchronize();
-    if( cudaSuccess != err )
-    {
-        fprintf( stderr, "cudaCheckError() with sync failed at %s:%i : %s\n",
-                 file, line, cudaGetErrorString( err ) );
-        exit( -1 );
-    }
-#endif
- 
-    return;
+return;
 }
